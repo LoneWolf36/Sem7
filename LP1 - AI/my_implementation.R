625b3dbb60b76e1e80a1fc081f295c44123da781
Faizan Khan <runefaizan36@gmail.com>
2018-09-03 10:23:37 +0530

    Update my_implementation.R

--- LP1 - AI/my_implementation.R
+++ LP1 - AI/my_implementation.R
@@ -9,8 +9,10 @@ my_split = sample.split(dataset, SplitRatio = 0.7)
 
 training_set = subset(dataset, my_split == TRUE)
 test_set = subset(dataset, my_split == FALSE)
+test_set$Salary = NA
 
 model = naiveBayes(dataset$Age, dataset$YearsExperience, dataset$Salary, training_set)
+results = predict(model, test_set)
 
 ggplot()
 geom_point(aes(training_set$YearsExperience, test_set$Salary), color = 'red')

6e802e3f7176d7ac62cfb6dd71e5153c2927df0a
LoneWolf36 <runefaizan36@gmail.com>
2018-09-25 10:20:00 +0530

    Updates

--- HPC/MatrixVector Multiplication/matV.cpp
+++ HPC/MatrixVector Multiplication/matV.cpp
@@ -7,6 +7,7 @@ int main() {
 	int size = 1000;
 	int matrix[size][size], vector[size], result[size];
 	int i, j;
+	
 	// Setting values
 	for(i=0;i<size;i++) {
 		for(j=0;j<size;j++) {
@@ -15,21 +16,38 @@ int main() {
 		vector[i] = i*2;
 		result[i] = 0;
 	}
+	
 	// Serial matrix multiplication
 	double start = omp_get_wtime();
 	for(i=0;i<size;i++) {
 		for(j=0;j<size;j++) {
 			result[i] += matrix[i][j] * vector[j];
 		}
-		cout<<result[i]<<endl;
 	}
 	double end = omp_get_wtime();
 	double elapsed_ser = end - start;
 	cout<<"Elapsed time for serial: "<<elapsed_ser;
 	
-	#pragma omp parallel
-	{
-		
+	// Setting result to 0 again
+	for(i=0;i<size;i++) {
+		result[i] = 0;
 	}
+	
+	// Parallel matrix multiplication
+	start = omp_get_wtime();
+	#pragma omp parallel shared (matrix, vector, result) private (i, j)
+	{
+		#pragma omp for schedule(static)
+		{
+			for(i=0;i<size;i++) {
+				for(j=0;j<size;j++) {
+					result[i] += matrix[i][j] * vector[j];
+				}
+			}
+		}	
+	}
+	end = omp_get_wtime();
+	double elapsed_par = end - start;
+	cout<<"Elapsed time for parallel: "<<elapsed_par;
 	return 0;
 }

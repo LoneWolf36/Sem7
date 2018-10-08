5be892238498d2091f82e06e537a8e87ee8f483a
Faizan Khan <runefaizan36@gmail.com>
2018-10-08 10:33:40 +0530

    Updates

--- DA/IRIS/iris.py
+++ DA/IRIS/iris.py
@@ -1,7 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn import datasets
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
 
 iris_dataset = datasets.load_iris()
 X = iris_dataset.data[:, :2]

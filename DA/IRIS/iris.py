f370d0d12c0cad652c9176563ea93c82f797375e
student <runefaizan36@gmail.com>
2018-10-09 11:22:55 +0530

    Changes

--- DA/IRIS/iris.py
+++ DA/IRIS/iris.py
@@ -2,16 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from pandas.plotting import scatter_matrix
-from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 
 url = "iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
